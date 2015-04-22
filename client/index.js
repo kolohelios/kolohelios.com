593b4fc3d07e156430b28c2b3d96ab9060f37de9
Jon Edwards <jkedwards@me.com>
2015-04-21 20:53:54 -0700

    wire up angular and build project scaffolding

--- client/index.js
+++ client/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+angular.module('portfolio', [])
+.controller('projectView', ['$scope', function($scope){
+  $scope.projects = [
+    { name: 'Calculator',
+      desc: 'While at Coding House, I was asked to duplicate the appearance and function of Apple\'s calculator.',
+      screenshotthumb: ''
+    },
+    { name: 'Tower of Hanoi',
+      desc: '',
+      screenshotthumb: ''
+    },
+    { name: 'test2',
+      desc: '',
+      screenshotthumb: ''
+    }
+  ];
+}]);

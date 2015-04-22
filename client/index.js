1679a10226c5d278ca14d5a13b1b51f509d77270
Jon Edwards <jkedwards@me.com>
2015-04-22 00:15:52 -0700

    set up Google Analytics, add more project data, fix display issues

--- client/index.js
+++ client/index.js
@@ -1,19 +1,48 @@
 'use strict';
 
 angular.module('portfolio', [])
-.controller('projectView', ['$scope', function($scope){
-  $scope.projects = [
+.run(['$rootScope', function($rootScope){
+  $rootScope.view = -1;
+  $rootScope.showProjectView = false;
+  $rootScope.projects = [
     { name: 'Calculator',
       desc: 'While at Coding House, I was asked to duplicate the appearance and function of Apple\'s calculator.',
-      screenshotthumb: ''
+      extddesc: 'After matching the styling of the Apple calculator, I was able to match the functionality of it.',
+      screenshotthumb: '/assets/calcthumb.jpg',
+      techs: ['JavaScript', 'jQuery', 'HTML5', 'CSS3'],
+      role: 'sole developer',
+      screenshot: '/assets/calcthumb.jpg',
+      linktext: 'Click to view calculator',
+      linktoapp: 'http://calc.kolohelios.com'
     },
     { name: 'Tower of Hanoi',
-      desc: '',
-      screenshotthumb: ''
+      desc: 'Another assignment at Coding House was to write a Tower of Hanoi game.',
+      extddesc: '',
+      screenshotthumb: '/assets/hanoithumb.jpg',
+      techs: ['JavaScript', 'jQuery', 'HTML5', 'CSS3'],
+      role: 'sole developer',
+      screenshot: '/assets/hanoithumb.jpg',
+      linktext: 'Click to play game',
+      linktoapp: 'http://hanoi.kolohelios.com/'
     },
-    { name: 'test2',
+    { name: 'Marmot Checkers',
       desc: '',
-      screenshotthumb: ''
+      extddesc: '',
+      screenshotthumb: '/assets/checkersthumb.jpg',
+      techs: [],
+      role: 'sole developer',
+      screenshot: '/assets/checkersthumb.jpg',
+      linktext: 'Click to play game',
+      linktoapp: 'http://checkers.kolohelios.com'
     }
   ];
+}])
+.controller('projectList', ['$rootScope', '$scope', function($rootScope, $scope){
+  $scope.caseStudy = function(projectNum){
+    $rootScope.view = projectNum;
+    $rootScope.showProjectView = true;
+  };
+}])
+.controller('projectView', ['$rootScope', '$scope', function($rootScope, $scope){
+
 }]);
